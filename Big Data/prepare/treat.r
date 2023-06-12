eb77b97b89761a8f99e2ae00afe277526eb0b326
Epwo <ewann.postic@hotmail.fr>
2023-06-12 14:46:33 +0200

    update gitignore

--- Big Data/prepare/treat.r
+++ Big Data/prepare/treat.r
@@ -1,10 +1,10 @@
-data <- read.csv("Big Data\\csvSource.csv",sep=";")
 #On cherche a retrouver les lignes = 'rows' qui sont vides is.na()
+data <- read.csv("Big Data\\csvSource.csv",sep=";")
 missing_rows <- apply(data, 1, function(row) any(row = 'null'))
+#on va procéder element par element, car chaque cas est différent.
+print(nrow(data))
+sousDF <- subset(data, data$place == "NULL")
+print(nrow(sousDF))
 
 
-sousDF <- subset(data,descr_athmo == "Neige – grêle")
 sousDF
-
-#dh <- data[data$descr_athmo == "Neige – grêle",]
-#dh
