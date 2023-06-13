cfeef49bfd83b7d8de2290e4c7fb9726c3c7e252
Epwo <ewann.postic@hotmail.fr>
2023-06-13 15:24:08 +0200

    update agglo fct

--- Big Data/prepare/preparation/maintestprep2.r
+++ Big Data/prepare/preparation/maintestprep2.r
@@ -1,5 +1,6 @@
 source("Big Data\\prepare\\preparation\\catVehic.R")
 data <- read.csv("Big Data\\csvSource.csv", sep = ";")
-final_data <- PrepCatVeh(final_data,final_data)
+finalData <- data
+final_data <- PrepCatVeh(data,final_data)
 print(nrow(final_data))
 write.csv(final_data,"Big Data\\tempcsv.csv",row.names=FALSE)
