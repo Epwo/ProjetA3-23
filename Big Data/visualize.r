f73e5a8aff41bfda876832d69d6e293fa544cc3b
philippineangbd <philippine.angibaud@isen-ouest.yncrea.fr>
2023-06-13 11:00:09 +0200

    fonction

--- Big Data/visualize.r
+++ Big Data/visualize.r
@@ -8,7 +8,20 @@ graphique_bar <- function(var1, var2) {
     deux_colonnes <- dataframe1[, c(var1, var2)]
     bar_plot <- ggplot(deux_colonnes, aes(x = dataframe1[[var1]], y = dataframe1[[var2]])) + # nolint: line_length_linter.
     geom_bar(stat = "identity", fill = "#2e6694") +
-    labs(x = var1, y = var2, title = paste("Nombre d'accidents en fonction de/des", var2)) # nolint: line_length_linter.
+    labs(x = var1, y = var2, title = paste("Nombre d'accidents en fonction de", var2)) + # nolint: line_length_linter.
+    theme_minimal() + theme(
+    axis.title.x = element_text(size = 25, face = "bold"),
+    axis.title.y = element_text(size = 25, face = "bold"),
+    panel.grid.major = element_line(
+            colour = "dodgerblue",
+            size = 0.5, linetype = "dotdash"
+        ),
+    axis.text = element_text(
+            size = 25, face = "bold"
+        ),
+     plot.title = element_text(colour = "red", face = "bold", size = 25, hjust = 0.5),
+    )
+
     print(bar_plot)
 }
 
@@ -34,8 +47,8 @@ histograme <- function(var1, var2) {
 }
 
 
-categorie <- c("A", "B", "C", "D", "C")
-valeur1 <- c(10, 20, 15, 5, 3)
+categorie <- c("A", "B", "C", "D", "C", "A")
+valeur1 <- c(10, 20, 15, 5, 3,1)
 valeur2 <- c(5, 15, 10, 20)
 
 dataframe1 <- data.frame(categorie = categorie, taille = valeur1)
