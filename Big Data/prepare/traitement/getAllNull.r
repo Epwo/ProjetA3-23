7fa0cc69e0284756dfc8701c2ad4f6dccb64feb2
Epwo <ewann.postic@hotmail.fr>
2023-06-13 11:28:25 +0200

    traitement age (-14 car df 2009) + minor conducteu

--- Big Data/prepare/traitement/getAllNull.r
+++ Big Data/prepare/traitement/getAllNull.r
@@ -1,13 +1,21 @@
 data <- read.csv("Big Data\\csvSource.csv",sep=";")
 #isnumeric()
-for (col_name in colnames(data)) {
-    sousDF <- subset(data, data[[col_name]] == "NULL")
-    if(nrow(sousDF) > 0){
-        print(col_name)
-        print(nrow(sousDF))
-        print('--')
+printNull <- function() {
+    for (col_name in colnames(data)) {
+        sousDF <- subset(data, data[[col_name]] == "NULL")
+        if(nrow(sousDF) > 0){
+            print(col_name)
+            print(nrow(sousDF))
+            print('--')
+        }
     }
 }
+printMinors <- function() {
+    sousDF <- subset(data, as.numeric(data$age) < 16 & data$descr_grav == "Tué")
+
+}
+
+printMinors()
 
 
 
