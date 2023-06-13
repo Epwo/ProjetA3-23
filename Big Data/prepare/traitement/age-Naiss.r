6d8ece5565b3b13a69d068c6d7ba6cfc29875635
Epwo <ewann.postic@hotmail.fr>
2023-06-13 14:06:19 +0200

    fixed return smaller sized outputcsv

--- Big Data/prepare/traitement/age-Naiss.r
+++ Big Data/prepare/traitement/age-Naiss.r
@@ -3,11 +3,12 @@ TreatAgeAnnNaiss <- function(dataInput, finalData) {
     #on pourrait remplacer ces valeur par la médiane
     #mais pour uniquement 3 valeurs, on décide de simplement les supprimer
     sousDF <- subset(dataInput, dataInput$age != "NULL")
+    #le df étant de 2009, et l'age étant calculé a partir de la date actuelle -> - 14
     sousDF$age <- (as.numeric(sousDF$age) - 14)
-    underDF <- subset(sousDF,!(sousDF$age < 12 & sousDF$place == 1))
+    #on enleve également les données des enfants de moins de 5 ans conducteur que je considere comme abbérante
+    underDF <- subset(sousDF,!(sousDF$age < 5 & sousDF$place == 1))
     finalData <- underDF
-
-    #les val NULL obtenues pour 'age' sont pour la meme ligne que 'an_nais'
+    #les val NULL obtenues pour 'age' sont pour la meme ligne que 'an_nais', donc ces deux val NULL sont suppr
     return(finalData)
 }
 
--- Big Data/prepare/traitement/lat-long.r
+++ Big Data/prepare/traitement/lat-long.r
@@ -1,16 +1,7 @@
 TreatLongLat <- function(dataInput, finalData) {
+    source("Big Data\\prepare\\traitement\\quartierData.r")
   # Create a new data frame where dataInput$ville starts with "PARIS"
-    sousDF <- subset(dataInput, startsWith(dataInput$ville, "PARIS"))
-
-    # Coordonnées de latitude et longitude pour chaque quartier
-    latitudes <- c(48.8626, 48.8691, 48.8637, 48.8566, 48.8448, 48.8493, 48.8567, 48.8722, 48.8760, 48.8761,
-                    48.8584, 48.8352, 48.8283, 48.8296, 48.8412, 48.8600, 48.8872, 48.8925, 48.8817, 48.8632)
-    longitudes <- c(2.3364, 2.3473, 2.3590, 2.3522, 2.3551, 2.3324, 2.3208, 2.3124, 2.3375, 2.3602,
-                    2.3795, 2.4458, 2.3627, 2.3267, 2.3002, 2.2620, 2.3075, 2.3444, 2.3834, 2.4001)
-
-    # Création du dictionnaire des quartiers de Paris
-    quartiers_paris <- setNames(Map(function(lat, long) c(lat = lat, long = long), latitudes, longitudes),
-                                paste0(1:20))
+    sousDF <- dataInput
 
 
     # Replace latitude and longitude values in sousDF
@@ -18,12 +9,29 @@ TreatLongLat <- function(dataInput, finalData) {
     for (i in seq_along(sousDF$ville)) {
         #on definit ville comme nom de la ville
         ville <- sousDF$ville[i]
-        #on recupere le num de quartier, en enlevant "PARIS" du nom de la ville -> 'PARIS 13' -> '13'
-        quartier_num <- as.numeric(sub("PARIS ", "", ville))
-        #on remplace les valeurs correspondantes grace au dict, quartier_paris
-        sousDF$latitude[i] <- quartiers_paris[[quartier_num]]["lat"]
-        sousDF$longitude[i] <- quartiers_paris[[quartier_num]]["long"]
+        if(startsWith(ville,"PARIS")){
+            #on recupere le num de quartier, en enlevant "PARIS" du nom de la ville -> 'PARIS 13' -> '13'
+            quartier_num <- as.numeric(sub("PARIS ", "", ville))
+            #on remplace les valeurs correspondantes grace au dict, quartier_paris
+            sousDF$latitude[i] <- arrondissements_paris[[quartier_num]]["lat"]
+            sousDF$longitude[i] <- arrondissements_paris[[quartier_num]]["long"]
+        }
+        else if(startsWith(ville,"MARSEILLE")){
+            #on recupere le num de quartier, en enlevant "PARIS" du nom de la ville -> 'PARIS 13' -> '13'
+            quartier_num <- as.numeric(sub("MARSEILLE ", "", ville))
+            #on remplace les valeurs correspondantes grace au dict, quartier_paris
+            sousDF$latitude[i] <- arrondissements_marseille[[quartier_num]]["lat"]
+            sousDF$longitude[i] <- arrondissements_marseille[[quartier_num]]["long"]
+        }
+        else if(startsWith(ville,"LYON")){
+            #on recupere le num de quartier, en enlevant "PARIS" du nom de la ville -> 'PARIS 13' -> '13'
+            quartier_num <- as.numeric(sub("LYON ", "", ville))
+            #on remplace les valeurs correspondantes grace au dict, quartier_paris
+            sousDF$latitude[i] <- arrondissements_lyon[[quartier_num]]["lat"]
+            sousDF$longitude[i] <- arrondissements_lyon[[quartier_num]]["long"]
+        }
     }
+    
 
     return(sousDF)
 }
