0e70296ec26e0d39925d868815f33ee1dca72e4c
Epwo <ewann.postic@hotmail.fr>
2023-06-14 16:21:05 +0200

    new csv all unm + created tableau croisé

--- Big Data/analyze/TabCrois.r
+++ Big Data/analyze/TabCrois.r
@@ -1,22 +1,7 @@
-library(ggplot2)
-
-tableau_croise <- function(df, var1, var2, outputName) {
-  # Calcul du tableau croisé
-  table <- table(df[[var1]], df[[var2]])
-
-  # Conversion du tableau croisé en un dataframe
-  df_table <- as.data.frame(table)
-
-  # Renommer les colonnes du dataframe
-  colnames(df_table) <- c(var1, var2, "count")
-
-  # Création du graphique à barres
-  plot <- ggplot(df_table, aes_string(x = var1, y = "count", fill = var2)) +
-    geom_bar(stat = "identity", position = "stack")
-
-  # Sauvegarde du graphique dans un fichier PDF
-  path <- paste("Big Data/plots/", outputName, ".pdf", sep = '')
-  ggsave(path, plot)
-
-  return(table)
+library(gridExtra)
+tableau_croise <- function(df, var1, var2) {
+    table <- ftable(df[[var1]], df[[var2]])
+    # Create plot
+    write.table(table, file = "Big Data/plots/cc.pdf.txt", sep = ",", quote = FALSE, row.names = F)
+    return(table)
 }
--- Big Data/analyze/mainAnalyz.r
+++ Big Data/analyze/mainAnalyz.r
@@ -3,5 +3,5 @@ source("Big Data/analyze/TabCrois.r")
 
 data <- read.csv("Big Data/csvOutput.csv", sep = ",", stringsAsFactors = FALSE)
 
-resultat <- tableau_croise(data, "descr_dispo_secu", "descr_grav","tabcrois")
+resultat <- tableau_croise(data, "descr_dispo_secu", "descr_grav")
 print(resultat)
