1d8032713985e7927d5d9a8ff48e763600208ba2
MathieuGLT <mathieu.guilletgit config --global user.name MathieuGLTgit config --global user.name Mathieu>
2023-06-14 10:53:37 +0200

    tr

--- Big Data/prepare/preparation/format.r
+++ Big Data/prepare/preparation/format.r
@@ -1,9 +1,12 @@
 PrepFormat <- function(dataInput){
     for (i in seq_along(dataInput$date)){
-        allDate <- dataInput$date[i]
-        dataInput$date[i] <- as.Date(allDate,format = "%d/%m/%Y")
+        #allDate <- dataInput$date[i]
+        #dataInput$date[i] <- as.Date(allDate,format = "%H %d/%m/%Y")
+        eachCodInsee <- dataInput$id_code_insee[i]
+        dataInput$id_code_insee[i] <- as.numeric(eachCodInsee)
+
     }
-    print(typeof(dataInput$date))
+    print(typeof(dataInput$id_code_insee))
     return(dataInput)
     
 }
