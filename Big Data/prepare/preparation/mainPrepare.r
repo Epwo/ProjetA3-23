fa8267a28f4e4c5650b5e56b869ea464db86d5f6
Epwo <ewann.postic@hotmail.fr>
2023-06-14 09:14:01 +0200

    corrected catVehic + added descr_grav

--- Big Data/prepare/preparation/mainPrepare.r
+++ Big Data/prepare/preparation/mainPrepare.r
@@ -2,11 +2,14 @@
 source("Big Data\\prepare\\preparation\\agglo.r")
 source("Big Data\\prepare\\preparation\\athmo.r")
 source("Big Data\\prepare\\preparation\\lumi.r")
+source("Big Data\\prepare\\preparation\\catVehic.R")
+source("Big Data\\prepare\\preparation\\gravite.r")
 #modification des colomnes du csv, fonction par fonction
-finalData <- read.csv("Big Data\\csvOutTrait.csv", sep = ",")
-finalData <- PrepCatVeh(finalData,finalData)
+data <- read.csv("Big Data\\csvOutTrait.csv", sep = ",")
+finalData <- PrepCatVeh(data,data)
 print(nrow(finalData))
-
+finalData <- gravite2num(finalData)
+print(nrow(finalData))
 #afin d'être certain de ne pas perdre de valeurs, je print la taille des lignes
 #on écrit dans csvOutput la nouvelle base de données
 write.csv(finalData,"Big Data\\csvOutput.csv",row.names=FALSE)
