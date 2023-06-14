ae7c3f46a5a53cbf2b0172857346a7d5dbcd83d8
Epwo <ewann.postic@hotmail.Fr>
2023-06-14 18:51:03 +0200

    cleaned workspace - changed all  \\ into /

--- Big Data/prepare/traitement/lat-long.r
+++ Big Data/prepare/traitement/lat-long.r
@@ -14,22 +14,22 @@ TreatLongLat <- function(dataInput, finalData) {
             #on recupere le num de quartier, en enlevant "PARIS" du nom de la ville -> 'PARIS 13' -> '13'
             quartier_num <- as.numeric(sub("PARIS ", "", ville))
             #on remplace les valeurs correspondantes grace au dict, quartier_paris
-            sousDF$latitude[i] <- arrondissements_paris[[quartier_num]]["lat"]
-            sousDF$longitude[i] <- arrondissements_paris[[quartier_num]]["long"]
+            sousDF$latitude[i] <- arrondissements_paris[[quartier_num]]["lat"] # nolint
+            sousDF$longitude[i] <- arrondissements_paris[[quartier_num]]["long"]# nolint
         }
         else if(startsWith(ville,"MARSEILLE")){
             #on recupere le num de quartier, en enlevant "PARIS" du nom de la ville -> 'PARIS 13' -> '13'
             quartier_num <- as.numeric(sub("MARSEILLE ", "", ville))
             #on remplace les valeurs correspondantes grace au dict, quartier_paris
-            sousDF$latitude[i] <- arrondissements_marseille[[quartier_num]]["lat"]
-            sousDF$longitude[i] <- arrondissements_marseille[[quartier_num]]["long"]
+            sousDF$latitude[i] <- arrondissements_marseille[[quartier_num]]["lat"]# nolint
+            sousDF$longitude[i] <- arrondissements_marseille[[quartier_num]]["long"]# nolint
         }
         else if(startsWith(ville,"LYON")){
             #on recupere le num de quartier, en enlevant "PARIS" du nom de la ville -> 'PARIS 13' -> '13'
             quartier_num <- as.numeric(sub("LYON ", "", ville))
             #on remplace les valeurs correspondantes grace au dict, quartier_paris
-            sousDF$latitude[i] <- arrondissements_lyon[[quartier_num]]["lat"]
-            sousDF$longitude[i] <- arrondissements_lyon[[quartier_num]]["long"]
+            sousDF$latitude[i] <- arrondissements_lyon[[quartier_num]]["lat"]# nolint
+            sousDF$longitude[i] <- arrondissements_lyon[[quartier_num]]["long"]# nolint
         }
     }
     
--- Big Data/prepare/traitement/mainTrait.R
+++ Big Data/prepare/traitement/mainTrait.R
@@ -1,10 +1,10 @@
-source("Big Data\\prepare\\traitement\\place.r")
-source("Big Data\\prepare\\traitement\\lat-long.r")
-source("Big Data\\prepare\\traitement\\age-Naiss.r")
-source("Big Data\\prepare\\traitement\\isInside.r")
+source("Big Data/prepare/traitement/place.r")
+source("Big Data/prepare/traitement/lat-long.r")
+source("Big Data/prepare/traitement/age-Naiss.r")
+source("Big Data/prepare/traitement/isInside.r")
 #On cherche a retrouver les lignes = 'rows' qui sont vides is.na()
 #on va procéder element par element, car chaque cas est différent.
-data <- read.csv("Big Data\\csvSource.csv",sep=";")
+data <- read.csv("Big Data/csvSource.csv",sep=";")
 #init finaldata
 finalData <- data
 print(nrow(finalData))
@@ -17,4 +17,4 @@ print(nrow(finalData))
 #en realité je pense qu'il n'est pas nécéssaire de donenr deux arg en entrée
 finalData <- CheckInFrance(finalData)
 print(nrow(finalData))
-write.csv(finalData,"Big Data\\csvOutTrait.csv",row.names=FALSE)
+write.csv(finalData,"Big Data/csvOutTrait.csv",row.names=FALSE)
