033dd66e48f17f021b41b5b2d2799d32cc76617c
Epwo <ewann.postic@hotmail.Fr>
2023-06-14 19:02:47 +0200

    done reglin by week, from the same funct

--- Big Data/analyze/regLin/RegLinMois.r
+++ Big Data/analyze/regLin/RegLinMois.r
@@ -1,37 +1,36 @@
-library(dplyr)  # Pour la manipulation des données
-library(ggplot2)  # Pour la visualisation des données
-library(gridExtra)
+library(dplyr)
+library(ggplot2)
+printRegLinMonth <- function(data, name, by) {
+  # Convertir la colonne 'date' en format de date
+  data$date <- as.Date(data$date)
 
-printRegLinMonth <- function(data,name){
+  # Créer une nouvelle colonne pour le regroupement
+  if (by == 'month') {
+    data$group <- format(data$date, "%m")
+  } else if (by == 'week') {
+    data$group <- format(data$date, "%W")
+  } else {
+    stop("Valeur invalide pour le paramètre 'by'. Attendu : 'month' ou 'week'.")
+  }
 
-    # Convertir la colonne 'date' en format de date
-    data$date <- as.Date(data$date)
+  # Regrouper les données et compter le nombre d'accidents
+  accidents_data <- data %>%
+    group_by(group) %>%
+    summarise(number_of_accidents = n())
 
-    # Créer une nouvelle colonne 'mois' à partir de la colonne 'date'
-    data$mois <- format(data$date, "%m")
+  # Ajuster un modèle de régression linéaire
+  model <- lm(number_of_accidents ~ as.numeric(group), data = accidents_data)
 
-    # Regrouper les données par mois et compter le nombre d'accidents par mois
-    accidents_par_mois <- data %>%
-    group_by(mois) %>% # nolint: object_usage_linter.
-    summarise(nombre_accidents = n())
+  # Créer le graphique
+  plot <- ggplot(accidents_data, aes(x = group, y = number_of_accidents)) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+    geom_text(aes(label = paste("y =", round(coef(model)[1], 2), "+", round(coef(model)[2], 2), "x")), x = Inf, y = -Inf, hjust = 1, vjust = 0, size = 4) +
+    geom_abline(intercept = coef(model)[1], slope = coef(model)[2], color = "red") +
+    labs(x = ifelse(by == 'month', "Mois", "Semaine"),
+         y = "Nombre d'accidents",
+         title = paste("Régression linéaire du nombre d'accidents par", by))
 
-    # Convert the month column to factor and reorder the levels
-    accidents_par_mois$mois <- factor(accidents_par_mois$mois, levels = unique(accidents_par_mois$mois))
-
-    # Ajuster un modèle de régression linéaire
-    modele <- lm(as.numeric(nombre_accidents) ~ as.numeric(mois), data = accidents_par_mois)
-
-
-
-    # Tracer un graphique de dispersion avec la ligne de régression
-    plot <- ggplot(accidents_par_mois, aes(x = mois, y = nombre_accidents)) + # nolint: object_usage_linter.
-        geom_point() +
-        geom_smooth(method = "lm", se = FALSE) +
-        geom_text(aes(label = paste("y =", round(coef(modele)[1], 2), "+", round(coef(modele)[2], 2), "x")), x = Inf, y = -Inf, hjust = 1, vjust = 0, size = 4) +
-        geom_abline(intercept = coef(modele)[1] , slope = coef(modele)[2], color = "red") +
-        labs(x = "Mois", y = "Nombre d'accidents", title = "Régression linéaire du nombre d'accidents par mois")
-
-
-    # Sauvegarder le graphique en PDF
-    ggsave(paste("Big Data/plots/",name,".pdf",sep=""), plot, width = 8, height = 6)
+  # Sauvegarder le graphique en fichier PDF
+  ggsave(paste("Big Data/plots/", name, ".pdf", sep = ""), plot, width = 8, height = 6)
 }
--- Big Data/analyze/regLin/mainRegLin.r
+++ Big Data/analyze/regLin/mainRegLin.r
@@ -1,3 +1,4 @@
 source("Big Data/analyze/regLin/RegLinMois.r")
 finalData <- read.csv("Big Data/csvOutputNum.csv")
-printRegLinMonth(finalData,name='regLinMonth')
+printRegLinMonth(finalData,name='regLinMonth',by='month')
+printRegLinMonth(finalData,name='regLinWeek',by='month')
