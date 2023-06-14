f551cbbf171d996438ef0c008c3c1114d962fa77
Epwo <ewann.postic@hotmail.fr>
2023-06-14 11:47:24 +0200

    corrected formzt for insee int + date

--- Big Data/prepare/preparation/format.r
+++ Big Data/prepare/preparation/format.r
@@ -1,18 +1,14 @@
-PrepFormat <- function(dataInput){
-    for (i in seq_along(dataInput$date)){
-        allDate <- dataInput$date[i]
-        dataInput$date[i] <- as.Date(allDate,format = "%Y-%m-%d %H:%M:%S")
-        eachCodInsee <- dataInput$id_code_insee[i]
-        dataInput$id_code_insee[i] <- as.integer(eachCodInsee)
-
-    }
-    return(dataInput)
-    
+PrepFormat <- function(dataInput) {
+    dataInput$date <- as.POSIXct(dataInput$date, format = "%Y-%m-%d %H:%M:%S")
+    dataInput$id_code_insee <- as.integer(dataInput$id_code_insee)
+  return(dataInput)
 }
+
 #print(typeof(data$date))
-finalData <- read.csv("Big Data\\csvOutTrait.csv", sep = ",")
+finalData <- read.csv("Big Data\\csvOutTrait.csv", sep = ",", stringsAsFactors = FALSE)
 finalData <- PrepFormat(finalData)
-print(typeof(finalData$date))
-print(typeof(finalData$id_code_insee))
+print(typeof(finalData$date[4]))
+print(typeof(finalData$id_code_insee[4]))
+print(finalData$date[4])
 write.csv(finalData,"Big Data\\temp.csv",row.names=FALSE)
 
