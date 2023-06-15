5ad6f736fb6d1d5ba8e558e0cfde2bd10b67d7bd
Epwo <ewann.postic@hotmail.Fr>
2023-06-15 17:24:25 +0200

    cleanup preparation in prepare/

--- Big Data/prepare/preparation/maintestprep2.r
+++
@@ -1,5 +0,0 @@
-source("Big Data\\prepare\\preparation\\catVehic.R")
-data <- read.csv("Big Data\\csvSource.csv", sep = ";")
-final_data <- PrepCatVeh(data,data)
-print(nrow(final_data))
-write.csv(final_data,"Big Data\\tempcsv.csv",row.names=FALSE)
