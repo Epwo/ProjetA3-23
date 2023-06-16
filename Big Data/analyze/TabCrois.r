ce1b74706fafe409a7241c23a5d9fdd2072d92a9
Epwo <ewann.postic@hotmail.Fr>
2023-06-16 07:04:40 +0200

    print rest of mosaique

--- Big Data/analyze/TabCrois.r
+++ Big Data/analyze/TabCrois.r
@@ -2,6 +2,6 @@ tableau_croise <- function(df, var1, var2) {
     table <- ftable(df[[var1]], df[[var2]])
     # Create plot
     cont <- stats:::format.ftable(table,quote=FALSE)
-    write.table(cont,sep=';',file = "Big Data/plots/crossTab.csv")
+    write.table(cont,sep=';',file = paste("Big Data/plots/crossTab",var1,"-",var2,".csv"))
     return(table)
 }
--- Big Data/analyze/mainAnalyz.r
+++ Big Data/analyze/mainAnalyz.r
@@ -5,6 +5,15 @@ library(ggplot2)
 data <- read.csv("Big Data/csvOutput.csv", sep = ",", stringsAsFactors = FALSE)
 
 resultat <- tableau_croise(data, "descr_dispo_secu", "descr_grav")
-png("Big Data/plots/mosaic.png")
-mosaicplot(resultat,shade=TRUE)
+png("Big Data/plots/mosaic_descrSecu-grav.png")
+mosaicplot(resultat,shade=TRUE,main = "My Mosaic - Securité vs gravité")
+
+resultat <- tableau_croise(data, "descr_lum", "descr_grav")
+png("Big Data/plots/mosaic_lumi-grav.png")
+mosaicplot(resultat,shade=TRUE,main = "My Mosaic - Luminosité vs gravité")
+
+resultat <- tableau_croise(data, "descr_cat_veh", "descr_grav")
+png("Big Data/plots/mosaic_vehic-grav.png")
+mosaicplot(resultat,shade=TRUE,main = "My Mosaic - Catégorie de Vehic. vs gravité")
+
 print(resultat)
